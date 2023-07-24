5eaf09a0d2357461e77340cde74f9f9896162ea8
Vrba Martin <Martin.Vrba@t-mobile.cz>
2023-07-24 19:54:12 +0200

    cviceni 5-3

--- src/main/java/org/example/product/ProductJpaRepository.java
+++ src/main/java/org/example/product/ProductJpaRepository.java
@@ -1,2 +1,8 @@
-package org.example.product;public interface ProductJpaRepository {
+package org.example.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductJpaRepository extends JpaRepository<Product, Long> {
 }
